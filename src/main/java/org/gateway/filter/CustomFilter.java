b05ca290108937f3d8b6cf05a5b380e8a9a9556d
nam.dinh <dragonsang10a1@gmail.com>
2024-03-27 16:56:56 +0700

    init

--- src/main/java/org/gateway/filter/CustomFilter.java
+++ src/main/java/org/gateway/filter/CustomFilter.java
@@ -2,8 +2,15 @@ package org.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
@@ -13,10 +20,27 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 		//Custom Pre-Filter. Suppose we can extract JWT and perform Authentication
 		return (exchange, chain) -> {
 			//Custom Post-Filter.Suppose we can call error response handler based on error code.
-			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-				System.out.println("Response status code: " + exchange.getResponse().getStatusCode());
-				System.out.println("Response headers: " + exchange.getResponse().getHeaders());
+			// from exachange object, i want to get the username and password
 
+			ServerHttpRequest request = exchange.getRequest();
+
+			Flux<DataBuffer> body = request.getBody();
+
+			// print request body
+			body.subscribe(buffer -> {
+				byte[] bytes = new byte[buffer.readableByteCount()];
+				buffer.read(bytes);
+				DataBufferUtils.release(buffer);
+				String bodyString = new String(bytes, StandardCharsets.UTF_8);
+				System.out.println(bodyString);
+			});
+
+			System.out.println(request);
+
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//				System.out.println("Response status code: " + exchange.getResponse().getStatusCode());
+//				System.out.println("Response headers: " + exchange.getResponse().getHeaders());
+				ServerHttpResponse response = exchange.getResponse();
 			}));
 		};
 	}
