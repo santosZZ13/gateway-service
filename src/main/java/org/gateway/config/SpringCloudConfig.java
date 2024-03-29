407632d9e03b57321d8ca64b1dd3e8e652cbe294
nam.dinh <dragonsang10a1@gmail.com>
2024-03-29 16:25:47 +0700

    init

--- src/main/java/org/gateway/config/SpringCloudConfig.java
+++ src/main/java/org/gateway/config/SpringCloudConfig.java
@@ -3,10 +3,15 @@ package org.gateway.config;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.gateway.filter.AuthenticationFilter;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Configuration
 @AllArgsConstructor
@@ -19,7 +24,11 @@ public class SpringCloudConfig {
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/api/auth/**")
-						.filters(f -> f.filter(authenticationFilter))
+						.filters(f -> f.filter(authenticationFilter)
+								.requestRateLimiter(c -> c
+										.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(keyResolver()))
+						)
 						.uri("http://localhost:8081/")
 						.id("gateway-service")
 				)
@@ -31,4 +40,17 @@ public class SpringCloudConfig {
 				.build();
 	}
 
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(10, 20);
+	}
+
+	@Bean
+	public KeyResolver keyResolver() {
+		return exchange -> {
+			log.info("KeyResolver: {}", exchange.getRequest().getRemoteAddress());
+			return Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getHostName());
+		};
+	}
 }
